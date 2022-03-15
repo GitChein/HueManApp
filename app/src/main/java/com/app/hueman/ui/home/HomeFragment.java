509e9b74862771624332e59a6e579fc59d0af8c6
Maor Amar <amarmaor@umich.edu>
2022-03-15 15:07:42 -0400

    [amarmaor][HM-2] Add to the home fragment's layout an imageView and two textViews. Added code to the home fragment's source code to allow a color to be selected from the color wheel.

--- app/src/main/java/com/app/hueman/ui/home/HomeFragment.java
+++ app/src/main/java/com/app/hueman/ui/home/HomeFragment.java
@@ -1,31 +1,63 @@
 package com.app.hueman.ui.home;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.app.hueman.R;
 import com.app.hueman.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    ImageView image;
+    TextView rgbText;
+    TextView colorBox;
+    Bitmap bm;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        image = root.findViewById(R.id.image);
+        rgbText = root.findViewById(R.id.rgbLabel);
+        colorBox = root.findViewById(R.id.colorBox);
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        image.setDrawingCacheEnabled(true);
+        image.buildDrawingCache(true);
+        image.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN ||
+                    motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
+                    bm = image.getDrawingCache();
+                    int rgb = bm.getPixel((int)motionEvent.getX(), (int)motionEvent.getY());
+                    int r = Color.red(rgb);
+                    int g = Color.green(rgb);
+                    int b = Color.blue(rgb);
+
+                    colorBox.setBackgroundColor(Color.rgb(r, g, b));
+
+                    rgbText.setText(r + " " + g + " " + b);
+                }
+                return true;
+            }
+        });
+
+
         return root;
     }
 
